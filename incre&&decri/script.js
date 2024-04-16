42b724ff8429c2dd0014b47a37402bc88227b013
saurav saha <sauravsaha024@gmail.com>
2024-04-16 12:44:07 +0530

    16-4

--- incre&&decri/script.js
+++ incre&&decri/script.js
@@ -2,10 +2,14 @@ const btn = document.querySelectorAll(".btn");
 const count = document.querySelector(".count");
 let cnt = 0;
 btn.forEach((btn) => {
+  //  This line adds a click event listener to each button. When a button is clicked, the function inside the listener is executed.
+
   btn.addEventListener("click", () => {
     if (btn.classList.contains("incre")) {
+      //if the clicked button has a class "incre". If it does, it increments the count (cnt++).
       cnt++;
     } else if (btn.classList.contains("decre")) {
+      //  This condition checks if the clicked button has a class "decre". If it does, it decrements the count (cnt--).
       cnt--;
     }
     count.textContent = cnt;
@@ -17,5 +21,7 @@ btn.forEach((btn) => {
     } else {
       count.style.color = "blue";
     }
+
+    //If cnt is greater than 0, the count text color becomes green. If cnt is less than 0, the count text color becomes red. If cnt is 0, the count text color becomes blue.
   });
 });
