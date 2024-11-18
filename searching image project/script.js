f8922f1c2ca8dea9f7b84ba8dd87532c8d8005e5
saurav saha <sauravsaha024@gmail.com>
2024-11-18 16:58:54 +0530

    still in progress

--- searching image project/script.js
+++ searching image project/script.js
@@ -1,19 +1,40 @@
+const accesskey = "oBgtpytb0KUXU4oAoiQmftNWDAEBJ1z4JqhVoKazpdo";
 const searchForm = document.querySelector("form");
 const imageContainer = document.getElementById("imageContainer");
-const searchInput = document.getElementById("searchInput");
+
 // <!-- api key      ->>>>>>>>>> **********  oBgtpytb0KUXU4oAoiQmftNWDAEBJ1z4JqhVoKazpdo --> <!-- https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY      -------  aunthentication -->
 // create a function*************
 
 // fetching image using api key
+// ivcUPE47C3RhWAVsvLNy6MwCdNH7N3oarYlisGWPRDE
+const fetchImage = async (query) => {
+  imageContainer.innerHTML = "";
+  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=25&client_id=${accesskey}`;
 
-const fetchImage = (query) => {
-  console.log(query);
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  console.log(data);
+
+  data.results.forEach((datas) => {
+    const url = datas.urls.regular;
+    const imageElement = document.createElement("div");
+    imageElement.classList.add("imgcls");
+    imageElement.innerHTML = `<img src="${url}"/>`;
+    // creating overley
+    const overleyElement = document.createElement("div");
+    overleyElement.classList.add("overley");
+
+    imageElement.appendChild(overleyElement);
+    imageContainer.appendChild(imageElement);
+  });
 };
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   //   console.log(searchInput.value);
-
+  const searchInput = document.getElementById("searchInput");
   const inputText = searchInput.value.trim();
 
   if (inputText !== "") {
@@ -22,3 +43,5 @@ searchForm.addEventListener("submit", (e) => {
     imageContainer.innerHTML = `<h2 style="color:red">Please type your search image</h2>`;
   }
 });
+
+/******************************************************************* another code*** */
