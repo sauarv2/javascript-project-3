83e39f2443b283d7206ac0579c504d18e3242fd1
saurav saha <sauravsaha024@gmail.com>
2024-12-08 02:30:55 +0530

    prograsing

--- currency converter/script.js
+++ currency converter/script.js
@@ -4,8 +4,21 @@ const AmouNt = document.getElementById("amount");
 const convertedAmout = document.getElementById("converted-amount");
 
 // make  arrey of country code**********************
-const countries = [{ Code: USD }, { Code: INR }];
+const countries = [
+  { Code: "USD" },
+  { Code: "INR" },
+  { Code: "EUR" },
+  { Code: "GBP" },
+];
 
 // select the country using for each loopp************
 
-c;
+countries.forEach((country) => {
+  const opt1 = document.createElement("option");
+  const opt2 = document.createElement("option");
+  opt1.value = opt2.value = country.Code;
+
+  opt1.textContent = opt2.textContent = `${country.Code}`;
+  fromAmout.appendChild(opt1);
+  console.log(toAmout.appendChild(opt2));
+});
