7bac2c43c2adee082d4b8eab8aaf6c7b12e619b4
saurav saha <sauravsaha024@gmail.com>
2024-12-08 01:51:18 +0530

    remain javascript part

---
+++ currency converter/script.js
@@ -0,0 +1,11 @@
+const fromAmout = document.querySelector(".fromAmount");
+const toAmout = document.querySelector(".toAmount");
+const AmouNt = document.getElementById("amount");
+const convertedAmout = document.getElementById("converted-amount");
+
+// make  arrey of country code**********************
+const countries = [{ Code: USD }, { Code: INR }];
+
+// select the country using for each loopp************
+
+c;
