beac140f6939479443e9ffeb8e04be17dbf912b0
saurav saha <sauravsaha024@gmail.com>
2025-04-12 02:50:27 +0530

    need woork and add more feature

--- charecter-counter/script.js
+++ charecter-counter/script.js
@@ -7,6 +7,9 @@ const space = document.querySelector(".totalSp");
 const linee = document.querySelector(".totalLi");
 const words = document.querySelector(".totalWd");
 const clear = document.querySelector(".clBtn");
+const copybtn = document.querySelector(".copyBtn");
+const sBtn = document.querySelector(".sectBtn");
+const remaing = document.querySelector(".totalRe");
 
 area.addEventListener("keyup", () => {
   let char = area.value;
@@ -28,12 +31,39 @@ area.addEventListener("keyup", () => {
   // no. of line ****************
   let linnn = char.split(/\r\n|\r|\n/).length;
   linee.innerText = `${linnn}`;
+
+  // charecter limit ******************
+  let charLimit = Number(500 - charLen);
+  if (charLimit < 0) {
+    charLimit = 0;
+  }
+  remaing.innerText = `${charLimit}`;
+
+  // if the charecter more than 500
+  if (char.length > 500) {
+    num.style.color = "red";
+  } else {
+    num.style.color = "white";
+  }
 });
 
 // clear the input filed*************************
 clear.addEventListener("click", () => {
   area.value = " ";
 });
+// copy the text**************************************
+copybtn.addEventListener("click", () => {
+  area.select();
+  // document.execCommand("copy");
+  navigator.clipboard.writeText(area.value);
+});
+
+sBtn.addEventListener("click", () => {
+  area.select();
+});
+
+// select the text*******************************
+
 // const CElement = document.createElement("div");
 // CElement.classList.add("totalCh");
 // CElement.innerHTML = `Totalcharecter of this text box ${area}`;
