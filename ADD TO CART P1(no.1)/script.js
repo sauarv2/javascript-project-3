01a78c330bdf8328277656b6b9ea7d6f607128e5
saurav saha <sauravsaha024@gmail.com>
2025-05-16 18:52:21 +0530

    new prc

--- ADD TO CART P1(no.1)/script.js
+++ ADD TO CART P1(no.1)/script.js
@@ -6,29 +6,14 @@ function Cart(item) {
   const cartID = document.getElementById("cartItem");
   // **********************
   addItem += 1;
-  const cartdiv = document.createElement("div");
-  cartdiv.classList.add("cartIMG");
-  cartdiv.setAttribute("id", addItem);
 
-  //   **** SET THE IMAGE ****************************
+  const html = `<div class= "cartIMG"   >
+              <img src="${item.children[0].currentSrc}"    />
+              <div class="card-title">${item.children[1].innerText}</div>
+              <div>
+                <label>size:${item.children[2].children[1].value}</label>`;
 
-  var image = document.createElement("img");
-  image.setAttribute("src", item.children[0].currentSrc);
-
-  //  *** SET THE IMAGE *************************
-  const cartTitle = document.createElement("div");
-
-  cartTitle.innerText = item.children[1].innerText;
-
-  //  *** SET THE LABEL *************************
-
-  const cartLabel = document.createElement("div");
-  cartLabel.innerText = item.children[2].innerText;
-
-  //  *** SET THE VALUE *************************
-
-  const select = document.createElement("span");
-  select.innerText = item.children[2].children[1].value;
+  cartID.insertAdjacentHTML("afterbegin", html);
 
   //  ***  DELETE THE ITEM **********************
   const deletetItem = document.createElement("button");
@@ -38,13 +23,6 @@ function Cart(item) {
   deletetItem.setAttribute("class", "btn btn-primary");
 
   //   CALL THE ALL ITEM TO DISPLAY BY CLICK ADD-TO-CART *********************
-
-  cartLabel.append(select);
-  cartID.append(cartdiv);
-  cartdiv.append(image);
-  cartdiv.append(cartTitle);
-  cartdiv.append(cartLabel);
-  cartdiv.append(deletetItem);
 }
 
 // *** Delete the item function***********************
